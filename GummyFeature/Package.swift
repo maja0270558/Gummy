6d178d2b08b0da409ffe80201e1da7c01c7265e8
django <maja0270558@gmail.com>
2024-07-31 09:29:34 +0800

    Update package

--- GummyFeature/Package.swift
+++ GummyFeature/Package.swift
@@ -5,30 +5,44 @@ import PackageDescription
 
 let package = Package(
     name: "GummyFeature",
-    platforms: [.iOS(.v16), .macOS(.v14)],
+    platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "GummyFeature",
-            targets: ["GummyFeature"]),
+        .library(name: "GummyFeature", targets: ["GummyFeature"]),
+
+        .library(name: "PlaybackService", targets: ["PlaybackService"]),
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture",
                  from: "1.11.2"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.5"),
+
+        .package(url: "https://github.com/spotify/ios-sdk", from: "2.1.6"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GummyFeature",
-            dependencies: [.tca]
+            dependencies: ["PlaybackService", .tca]
         ),
+
+        .target(
+            name: "PlaybackService",
+            dependencies: [.dependencies, .spotify]
+        ),
+
         .testTarget(
             name: "GummyFeatureTests",
-            dependencies: ["GummyFeature", .tca]),
-    ])
+            dependencies: ["GummyFeature", .tca]
+        ),
+    ]
+)
 
 extension Target.Dependency {
     static let tca = product(name: "ComposableArchitecture",
                              package: "swift-composable-architecture")
+
+    static let dependencies = product(name: "Dependencies",
+                                      package: "swift-dependencies")
+
+    static let spotify = product(name: "SpotifyiOS",
+                                 package: "ios-sdk")
 }
